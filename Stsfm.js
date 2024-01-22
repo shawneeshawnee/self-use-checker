f2dd4dcf1ac29260c35f4a527fac4ba3ccde4669
shawneeshawnee <63252093+shawneeshawnee@users.noreply.github.com>
2024-01-22 09:06:33 +0800

    Update Stsfm.js

--- Stsfm.js
+++ Stsfm.js
@@ -9,13 +9,27 @@ hostname = api.stats.fm
 
 */
 
-var guding = JSON.parse($response.body);
-guding.item.isPro = true;
-guding.item.isPlus = true;
-guding.item.hasSwipefy = true;
-guding.item.displayName = "Shannon";
-guding.item.customId = "https:\/\/t\.me\/Guding88";
-guding.item.profile.bio = "SelfUse!";
-guding.item.profile.theme = "blue";
-guding.item.image = "https:\/\/cdn.stats.fm\/file\/statsfm\/images\/users\/31tkfv4thwwc5czcbq24uyyjbtzq\/83be2cc47a458ffb84f0ad8a1ba3bba4.webp";
-$done({ body: JSON.stringify(guding) });
+// Parse the API response body
+const responseObj = JSON.parse($response.body);
+
+// Ensuring the 'item' object exists in the response
+if (responseObj && responseObj.item) {
+    // Updating properties of the 'item' object
+    responseObj.item.isPro = true;
+    responseObj.item.isPlus = true;
+    responseObj.item.hasSwipefy = true;
+    responseObj.item.displayName = "AA";
+    responseObj.item.customId = "https://t.me/Guding88";
+
+    // Ensuring the 'profile' object exists in the 'item'
+    if (responseObj.item.profile) {
+        responseObj.item.profile.bio = "!";
+        responseObj.item.profile.theme = "pink";
+    }
+
+    // Updating the image URL
+    responseObj.item.image = "https://cdn.stats.fm/file/statsfm/images/users/31tkfv4thwwc5czcbq24uyyjbtzq/83be2cc47a458ffb84f0ad8a1ba3bba4.webp";
+}
+
+// Finalize the response modification
+$done({ body: JSON.stringify(responseObj) });
